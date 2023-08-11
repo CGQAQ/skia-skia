26f03f85bc0578fe7f5e74ca97e23228a54bb92f
CGQAQ <m.jason.liu@outlook.com>
2023-08-11 11:19:17 +0800

    codec sources defs

---
+++ skia-cmake/src/codec.cmake
@@ -0,0 +1,142 @@
+# bazel //src/codec
+set(SKIA_CODEC_ROOT "${SKIA_ROOT_SRC_DIR}/codec")
+
+
+# CORE_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_CORE
+        "${SKIA_CODEC_ROOT}"
+        "SkCodec.cpp"
+        "SkCodecImageGenerator.cpp"
+        "SkCodecImageGenerator.h"
+        "SkCodecPriv.h"
+        "SkColorPalette.cpp"
+        "SkColorPalette.h"
+        "SkFrameHolder.h"
+        "SkMaskSwizzler.cpp"
+        "SkMaskSwizzler.h"
+        "SkMasks.cpp"
+        "SkMasks.h"
+        "SkPixmapUtils.cpp"
+        "SkPixmapUtilsPriv.h"
+        "SkSampler.cpp"
+        "SkSampler.h"
+        "SkSwizzler.cpp"
+        "SkSwizzler.h"
+)
+
+# DECODE_ANDROID_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_ANDROID
+        "${SKIA_CODEC_ROOT}"
+        "SkAndroidCodec.cpp"
+        "SkAndroidCodecAdapter.cpp"
+        "SkAndroidCodecAdapter.h"
+        "SkSampledCodec.cpp"
+        "SkSampledCodec.h"
+)
+
+# DECODE_BMP_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_BMP
+        "${SKIA_CODEC_ROOT}"
+        "SkBmpBaseCodec.cpp"
+        "SkBmpBaseCodec.h"
+        "SkBmpCodec.cpp"
+        "SkBmpCodec.h"
+        "SkBmpMaskCodec.cpp"
+        "SkBmpMaskCodec.h"
+        "SkBmpRLECodec.cpp"
+        "SkBmpRLECodec.h"
+        "SkBmpStandardCodec.cpp"
+        "SkBmpStandardCodec.h"
+        "SkWbmpCodec.cpp"
+        "SkWbmpCodec.h"
+)
+
+# DECODE_GIF_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_GIF
+        "${SKIA_CODEC_ROOT}"
+        "SkScalingCodec.h"
+        "SkWuffsCodec.cpp"
+)
+
+# DECODE_JPEG_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_JPEG
+        "${SKIA_CODEC_ROOT}"
+        "SkEncodedInfo.cpp"
+        "SkJpegCodec.cpp"
+        "SkJpegCodec.h"
+        "SkJpegConstants.h"
+        "SkJpegDecoderMgr.cpp"
+        "SkJpegDecoderMgr.h"
+        "SkJpegSourceMgr.cpp"
+        "SkJpegSourceMgr.h"
+        "SkJpegUtility.cpp"
+        "SkJpegUtility.h"
+        "SkParseEncodedOrigin.cpp"
+        "SkParseEncodedOrigin.h"
+)
+
+# DECODE_XMP_FILES
+set(
+        SKIA_CODEC_XMP_SRCS_FILES
+        "SkXmp.cpp"
+)
+foreach (SKIA_CODEC_XMP_SRCS_FILE ${SKIA_CODEC_XMP_SRCS_FILES})
+    list(APPEND SKIA_CODEC_XMP_SRCS "${SKIA_CODEC_ROOT}/${SKIA_CODEC_XMP_SRCS_FILE}")
+endforeach ()
+unset(SKIA_CODEC_XMP_SRCS_FILES)
+message(STATUS "SKIA_CODEC_XMP_SRCS: ${SKIA_CODEC_XMP_SRCS}")
+
+# DECODE_PNG_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_PNG
+        "${SKIA_CODEC_ROOT}"
+        "SkEncodedInfo.cpp"
+        "SkIcoCodec.cpp"
+        "SkIcoCodec.h"
+        "SkPngCodec.cpp"
+        "SkPngCodec.h"
+)
+
+# DECODE_RAW_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_RAW
+        "${SKIA_CODEC_ROOT}"
+        "SkRawCodec.cpp"
+        "SkRawCodec.h"
+)
+
+# DECODE_WEBP_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_WEBP
+        "${SKIA_CODEC_ROOT}"
+        "SkEncodedInfo.cpp"
+        "SkParseEncodedOrigin.cpp"
+        "SkParseEncodedOrigin.h"
+        "SkScalingCodec.h"
+        "SkWebpCodec.cpp"
+        "SkWebpCodec.h"
+)
+
+# DECODE_AVIF_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_AVIF
+        "${SKIA_CODEC_ROOT}"
+        "SkAvifCodec.cpp"
+        "SkAvifCodec.h"
+        "SkScalingCodec.h"
+)
+
+# DECODE_JXL_FILES
+split_srcs_and_hdrs_relative(
+        SKIA_CODEC_DECODE_JXL
+        "${SKIA_CODEC_ROOT}"
+        "SkJpegxlCodec.cpp"
+        "SkJpegxlCodec.h"
+        "SkScalingCodec.h"
+)
+
--- skia-cmake/src/common.cmake
+++ skia-cmake/src/common.cmake
@@ -19,3 +19,65 @@ function(generate_cpp_files_for_header_list name headers)
 
     set(${name} ${generated_cpp_files} PARENT_SCOPE)
 endfunction()
+
+
+# Example usage:
+#   set(files "SkAAClip.cpp" "SkAAClip.h")
+#   split_srcs_and_hdrs(MyGroup ${files})
+#   message(STATUS "Sources: ${MyGroup_SRCS}")
+#   message(STATUS "Headers: ${MyGroup_HDRS}")
+function(split_srcs_and_hdrs name files_list)
+
+    set(srcs_list "")
+    set(hdrs_list "")
+
+    foreach(file ${files_list})
+        if(${file} MATCHES "\\.cpp$")
+            list(APPEND srcs_list ${file})
+        elseif(${file} MATCHES "\\.mm$")
+            list(APPEND srcs_list ${file})
+        elseif(${file} MATCHES "\\.h$")
+            list(APPEND hdrs_list ${file})
+        else()
+            message(FATAL_ERROR "File is neither .cpp, .mm, nor .h: ${file}")
+        endif()
+    endforeach()
+
+    if((NOT srcs_list) OR (NOT hdrs_list))
+        message(FATAL_ERROR "The list consists of either only source or header files. No need to use this function.")
+    endif()
+
+    # Instead of creating filegroups, we set variables for use in parent scope
+    set(${name}_SRCS ${srcs_list} PARENT_SCOPE)
+    set(${name}_HDRS ${hdrs_list} PARENT_SCOPE)
+
+endfunction()
+
+
+# Example usage:
+#   split_srcs_and_hdrs_relative(DECODE_PNG BASE_DIR "${FILE_LIST}")
+#   message(STATUS "DECODE_PNG_SRCS: ${DECODE_PNG_SRCS}")
+#   message(STATUS "DECODE_PNG_HDRS: ${DECODE_PNG_HDRS}")
+function(split_srcs_and_hdrs_relative name base)
+    set("${name}_FILES" "")
+    set(
+            "${name}_FILENAMES"
+            ${ARGN}
+    )
+    foreach (FILE ${${name}_FILENAMES})
+        list(APPEND "${name}_FILES" "${base}/${FILE}")
+    endforeach()
+    split_srcs_and_hdrs(
+            ${name}
+            "${${name}_FILES}"
+    )
+
+    # TODO(CGQAQ): verbose mode only
+    message(STATUS "${name}_SRCS ${${name}_SRCS}")
+    message(STATUS "${name}_HDRS ${${name}_HDRS}")
+
+    set("${name}_SRCS" "${${name}_SRCS}" PARENT_SCOPE)
+    set("${name}_HDRS" "${${name}_HDRS}" PARENT_SCOPE)
+    unset("${name}_FILES")
+    unset("${name}_FILENAMES")
+endfunction()
